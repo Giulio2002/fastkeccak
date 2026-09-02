4db4d0cc46c8e5f83feb1ca2524895b152cb6a5f
Alexey Sharov <AskAlexSharov@gmail.com>
2026-09-02 11:43:21 +0700

    keccak: drop the benchmark sink
    b.Loop already keeps loop-body results alive on go1.24.

--- keccak_write_bench_test.go
+++ keccak_write_bench_test.go
@@ -5,8 +5,6 @@ import (
 	"testing"
 )
 
-var oneByteWriteDigest [32]byte
-
 func BenchmarkHasherOneByteWrites(b *testing.B) {
 	for _, size := range []int{32, 128, 256} {
 		b.Run(strconv.Itoa(size), func(b *testing.B) {
@@ -18,7 +16,7 @@ func BenchmarkHasherOneByteWrites(b *testing.B) {
 				for i := range data {
 					h.Write(data[i : i+1])
 				}
-				oneByteWriteDigest = h.Sum256()
+				h.Sum256()
 			}
 		})
 	}
